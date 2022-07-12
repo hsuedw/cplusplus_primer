58eb12e2d6a9878ad487ec7e05c14bad69ab1939
edward.hc.hsu <hsuedw@gmail.com>
2022-07-12 09:05:55 +0800

    Implement the solution for Exercise 1.20

---
+++ ch1/exercise1.20/exercise1.20.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item book;
+
+    std::cout << "Please enter the sales record: " << std::endl;
+    // read ISBN, number of copies sold, and sales price
+    while (std::cin >> book) {
+        std::cout << "ISBN, numberof copies sold, and total revenue, and average price: ";
+        std::cout << book << std::endl;
+    }
+
+    return 0;
+}
