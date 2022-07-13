122488c30b5f730285cc903b89cc6b1579196542
edward.hc.hsu <hsuedw@gmail.com>
2022-07-13 08:55:31 +0800

    Implement the solution for Exercise 1.25

---
+++ ch1/exercise1.25/exercise1.25.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item total;
+
+    if (std::cin >> total) {
+        Sales_item trans;
+        while (std::cin >> trans) {
+           if (compareIsbn(total, trans)) {
+                total += trans;
+            } else {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+	}
+        std::cout << total << std::endl;
+    } else {
+        std::cout << "No input data!" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
