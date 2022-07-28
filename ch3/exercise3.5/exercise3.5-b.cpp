0ff8b9d2bd0055c6eb717cbfea4734a0d559636b
edward.hc.hsu <hsuedw@gmail.com>
2022-07-28 08:57:29 +0800

    Implement the solution for Exercise 3.5

---
+++ ch3/exercise3.5/exercise3.5-b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string word, line;
+    while (cin >> word) {
+        line += word + " ";
+    }
+    cout << line << endl;
+
+    return 0;
+}
