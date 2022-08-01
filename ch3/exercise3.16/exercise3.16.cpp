1abe5d97d029e8049b13e92595545281ca83efcf
edward.hc.hsu <hsuedw@gmail.com>
2022-08-01 09:51:26 +0800

    Implement the solution for Exercise 3.16

---
+++ ch3/exercise3.16/exercise3.16.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+template <typename T> void print_vec(vector<T>& vec, const string& name) {
+    cout << "the size of " << name << ": " << vec.size() << endl;
+    if (!vec.empty()) {
+        cout << "the contents of " << name << ": ";
+        for (const auto &x : vec) {
+	    cout << "[" << x << "] ";
+	}
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> v1;
+    print_vec(v1, "v1");
+
+    vector<int> v2(10);
+    print_vec(v2, "v2");
+
+    vector<int> v3(10, 42);
+    print_vec(v3, "v3");
+
+    vector<int> v4{10};
+    print_vec(v4, "v4");
+
+    vector<int> v5{10, 42};
+    print_vec(v5, "v5");
+
+    vector<string> v6{10};
+    print_vec(v6, "v6");
+
+    vector<string> v7{10, "hi"};
+    print_vec(v7, "v7");
+
+    return 0;
+}
