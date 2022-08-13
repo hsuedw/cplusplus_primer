c01c624ccf75ec71499b2c7da004e24b753ae476
edward.hc.hsu <hsuedw@gmail.com>
2022-08-12 23:34:44 -0700

    Fix the solution for Exercise 4.7

--- ch4/exercise4.7/exercise4.7.cpp
+++ ch4/exercise4.7/exercise4.7.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main()
 {
     int x = INT_MAX;
-    cout << x << ", " << x + 1 << endl;
+    cout << "x = " << x << ", x + 1 = " << x + 1 << endl;
 
     int y = INT_MIN;
-    cout << y << ", " << y - 1 << endl;
+    cout << "y = " << y << ", y - 1 = " << y - 1 << endl;
 
     unsigned int z = UINT_MAX;
-    cout << z << ", " << z + 1 << endl;
+    cout << "z = " << z << ", z + 1 = " << z + 1 << endl;
 
     return 0;
 }
