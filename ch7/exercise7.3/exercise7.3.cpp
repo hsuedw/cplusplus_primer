23bec46abc6be15f50163a393b0c8b3870fe48e3
edward.hc.hsu <hsuedw@gmail.com>
2022-09-11 09:11:59 +0800

    Implement the solution for Exercise 7.3

---
+++ ch7/exercise7.3/exercise7.3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+// NOTE: The Sales_data class is not full developed yet.
+#include "Sales_data.h"
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main()
+{
+    Sales_data total;  // variable to hold data for the next transaction
+
+    // read the first transaction and ensure that there are data to process
+    if (read(cin, total)) {
+        Sales_data trans; // variable to hold the running sum
+
+	// read and process the remaining transactions
+	while (read(cin, trans)) {
+            // if we're still processing the same book
+            if (total.isbn() == trans.isbn()) {
+                total.combine(trans); // update the running total
+            } else {
+                // print results for the previous book
+                print(cout,total);
+                total = trans; // total now refers to the next book
+            }
+	}
+        print(cout, total); // print the last transaction
+    } else {
+        // no input! warn the user
+        cerr << "No data?!" << endl;
+	return -1;  // indicate failure
+    }
+
+    return 0;
+}
