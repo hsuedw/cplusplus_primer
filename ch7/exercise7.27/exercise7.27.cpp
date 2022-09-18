1721e7ab8416b7f0d70324f144e3ca0cfba5a9df
edward.hc.hsu <hsuedw@gmail.com>
2022-09-19 07:46:28 +0800

    Implement the solution for Exercise 7.27

---
+++ ch7/exercise7.27/exercise7.27.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Screen.h"
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main()
+{
+    Screen myScreen(5, 5, 'X');
+    myScreen.move(4, 0).set('#').display(cout);
+    cout << "\n";
+    myScreen.display(cout);
+    cout << "\n";
+
+    return 0;
+}
