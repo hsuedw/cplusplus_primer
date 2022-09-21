0c0cbd7a5d8b1de5aa7524b0831d9b147c09382c
edward.hc.hsu <hsuedw@gmail.com>
2022-09-22 07:44:59 +0800

    Implement the solution for Exercise 7.38

---
+++ ch7/exercise7.38/exercise7.38.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Sales_data.h"
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::istream;
+using std::ostream;
+
+int main()
+{
+    Sales_data d1;
+
+    print(cout, d1) << endl;
+
+    return 0;
+}
