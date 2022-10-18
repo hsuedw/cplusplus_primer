89070a2f782fe93e718e504bb23305c492a97853
edward.hc.hsu <hsuedw@gmail.com>
2022-10-18 09:03:55 +0800

    Implement the solution for Exercise 9.43

---
+++ ch9/exercise9.43/exercise9.43.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+void myReplace(string &s, const string &oldVal, const string &newVal)
+{
+    auto it = begin(s);
+    while (it < end(s) - oldVal.size() + 1) {
+        auto it2 = begin(oldVal);
+	for (auto it3 = it; it2 != end(oldVal); ++it2, ++it3) {
+            if (*it2 != *it3) {
+                break;
+            }
+	}
+
+	if (it2 == end(oldVal)) {
+            // In s, find a substing and it matches oldVal.
+            decltype(s.size()) pos = it - begin(s);
+	    s.erase(pos, oldVal.size());
+	    s.insert(pos, newVal);
+
+	    it = begin(s) + pos + newVal.size();
+	} else {
+            // In s, didn't find any substring that matches oldVal.
+            ++it;
+	}
+    }
+}
+
+int main()
+{
+    string s = "r u ok?\ngo thru\ntho tho altho\nthrough thruu";
+
+    cout << "old string: " << endl;
+    cout << s << endl;
+    cout << "------------------" << endl;
+
+    myReplace(s, "tho", "though");
+    cout << "Replace \"tho\" by \"though\": " << endl;
+    cout << s << endl;
+    cout << "------------------" << endl;
+
+    myReplace(s, "thru", "through");
+    cout << "Replace \"tho\" by \"though\": " << endl;
+    cout << s << endl;
+
+    return 0;
+}
