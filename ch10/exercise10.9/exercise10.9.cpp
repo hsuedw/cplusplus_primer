d01d3ebd8bba13a169af23fefe8d55b8a4992e1a
edward.hc.hsu <hsuedw@gmail.com>
2022-10-24 07:40:25 +0800

    Implement the solution for Exercise 10.9

---
+++ ch10/exercise10.9/exercise10.9.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+using std::vector;
+using std::string;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ostream;
+
+template <typename T> ostream &operator<<(ostream &os, vector<T> vec) 
+{
+    for (decltype(vec.size()) i = 0; i < vec.size(); ++i) {
+        cout << vec[i];
+	if (i + 1 != vec.size()) {
+            cout << " ";
+	}
+    }
+    return os;
+}
+
+void elimDups(vector<string> &words)
+{
+    sort(begin(words), end(words));
+
+    vector<int>::size_type i = 0, j = 1;
+    while (j < words.size()) {
+        if (words[i] != words[j]) {
+            ++i;
+	    swap(words[i], words[j]);
+	    ++j;
+	} else {
+            // words[i] == words[j] is true
+            ++j;
+        }
+    }
+
+    // words[i] is the last unique word.
+    // All the words beyond the index i are duplicated words.
+
+    // Use the pop_back member function to delete all the duplicated words.
+    while (words.size() > i + 1) {
+        words.pop_back();
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    vector<string> words = {"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle"};
+
+    elimDups(words);
+    cout << words << endl;
+
+    return 0;
+}
