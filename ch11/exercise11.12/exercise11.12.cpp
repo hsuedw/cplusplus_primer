90f43935b2e9ec1f4684e035fbf994204f290e81
edward.hc.hsu <hsuedw@gmail.com>
2022-11-05 20:42:25 +0800

    Implement the solution for Exercise 11.12

---
+++ ch11/exercise11.12/exercise11.12.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+using std::vector;
+using std::pair;
+using std::string;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ostream;
+
+int main(int argc, char *argv[])
+{
+    vector<pair<string, int>> data;
+    string word;
+    int num;
+    while (cin >> word >> num) {
+        data.push_back(make_pair(word, num));
+    }
+
+    for (const auto &p : data) {
+        cout << p.first << ": " << p.second << endl;
+    }
+
+    return 0;
+}
