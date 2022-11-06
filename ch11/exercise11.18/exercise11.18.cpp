c3f15b792508a54c8b1b0c20bd9d311e7a4d5fbb
edward.hc.hsu <hsuedw@gmail.com>
2022-11-06 10:28:15 +0800

    Implement the solution for Exercise 11.18

---
+++ ch11/exercise11.18/exercise11.18.cpp
@@ -0,0 +1,14 @@
+#include <map>
+#include <string>
+
+using std::map;
+using std::string;
+
+int main()
+{
+    map<string, size_t> word_count;
+
+    map<string, size_t>::iterator map_it = word_count.begin();
+
+    return 0;
+}
