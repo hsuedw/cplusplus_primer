e47f7a3b0adf93e3cfe3ff7b699ecf2fc4954eb8
edward.hc.hsu <hsuedw@gmail.com>
2022-12-12 10:47:26 +0800

    Implement the solution for Exercise 12.15

---
+++ ch12/exercise12.15/exercise12.15.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <memory>
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ostream;
+using std::shared_ptr;
+using std::make_shared;
+
+struct destination {};
+struct connection {};
+
+connection connect(destination *pd)
+{
+    cout << "Open a connection." << endl;
+    return connection();
+}
+
+void disconnect(connection c)
+{
+    cout << "Close a connection." << endl;
+}
+
+void f(destination &d)
+{
+    cout << "Use connection in f..." << endl;
+    connection c = connect(&d);
+
+    // forget to call disconnect to close the connection
+}
+
+void my_f(destination &d)
+{
+    cout << "Use connection in my_f..." << endl;
+    connection c = connect(&d);
+
+    // Use shared_ptr and deleter to manage connection.
+    shared_ptr<connection> p(&c, [](connection *p){ cout << "The connection is going to be closed." << endl; disconnect(*p); });
+}
+
+int main(int argc, char *argv[])
+{
+    destination d;
+
+    cout << "Test 1:" << endl;
+    f(d);
+
+    cout << endl << "Test 2:" << endl;
+    my_f(d);
+
+    return 0;
+}
