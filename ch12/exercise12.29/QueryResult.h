98172b21e872abed0e6eb9136551ffaa040bdc0a
edward.hc.hsu <hsuedw@gmail.com>
2022-12-29 10:43:45 +0800

    Implement the solution for Exercise 12.29

---
+++ ch12/exercise12.29/QueryResult.h
@@ -0,0 +1,39 @@
+#ifndef __QUERYRESULT_H__
+#define __QUERYRESULT_H__
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <memory>
+#include <iostream>
+
+#include "TextQuery.h"
+
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::set;
+using std::shared_ptr;
+using std::ostream;
+
+class QueryResult {
+    friend ostream &print(ostream &os, const QueryResult &qr);
+public:
+    QueryResult(string target,
+                shared_ptr<vector<string>> text,
+                shared_ptr<set<TextQuery::lineNumType>> lineNum) :
+        target(target),
+        text(text), 
+	lineNum(lineNum) {}
+
+private:
+    string target;
+    shared_ptr<vector<string>> text;
+    shared_ptr<set<TextQuery::lineNumType>>  lineNum;
+};
+
+
+
+
+#endif // __QUERYRESULT_H__
