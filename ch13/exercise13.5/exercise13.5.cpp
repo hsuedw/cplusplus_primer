f404ab8bfcd7188731e041bcb21b392e13085703
edward.hc.hsu <hsuedw@gmail.com>
2023-01-10 10:27:08 +0800

    Impelement the solution for Exercise 13.5

---
+++ ch13/exercise13.5/exercise13.5.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+class HasPtr {
+public:
+    HasPtr(const std::string &s = std::string()) :
+        ps(new std::string(s)), i(0) {}
+
+    // copy constructor
+    HasPtr(const HasPtr &rhs) :
+    ps(new std::string(*rhs.ps)), i(rhs.i) {}
+
+    std::string get() { return *ps; }
+    void set(const std::string &s) { *ps = s; }
+private:
+    std::string *ps;
+    int i;
+};
+
+
+int main()
+{
+    HasPtr obj1= std::string("Edward");
+    HasPtr obj2 = obj1;
+
+    std::cout << obj1.get() << " " << obj2.get() << std::endl;
+
+    obj2.set("Angus");
+    std::cout << obj1.get() << " " << obj2.get() << std::endl;
+
+
+    return 0;
+}
