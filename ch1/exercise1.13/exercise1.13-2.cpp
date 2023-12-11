03b801b5daa01a9982f63a2256508980a682e5e3
edward.hc.hsu <hsuedw@gmail.com>
2023-12-11 13:16:32 +0800

    Update the solution of Exercise 1.13-2

--- ch1/exercise1.13/exercise1.13-2.cpp
+++ ch1/exercise1.13/exercise1.13-2.cpp
@@ -3,7 +3,10 @@
 int main()
 {
     for (int i = 10; i >= 0; --i) {
-        std::cout << i << " ";
+        std::cout << i;
+	if (i != 0) {
+            std::cout << ", ";
+	}
     }
     std::cout << std::endl;
 
