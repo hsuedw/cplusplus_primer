4d6a224dcbc58c4485df3754764fcef10c3aee1f
edward.hc.hsu <hsuedw@gmail.com>
2023-12-11 11:41:07 +0800

    Update the code in Exercise 1.10

--- ch1/exercise1.10/exercise1.10.cpp
+++ ch1/exercise1.10/exercise1.10.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-int main()
-{
-    int i = 10;
-    while (i >= 0) {
-        std::cout << i-- << " ";
+int main() {
+    int n = 10;
+
+    while (n >= 0) {
+        std::cout << n;
+	if (n != 0) {
+            std::cout << ", ";
+	}
+	--n;
     }
     std::cout << std::endl;
 
