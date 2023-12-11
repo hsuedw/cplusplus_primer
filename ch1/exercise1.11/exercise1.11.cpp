bdc15d347cf9e1837d11139464aa3aaa39ede7e7
edward.hc.hsu <hsuedw@gmail.com>
2023-12-11 11:46:54 +0800

    Update the code of Exercise 1.11

--- ch1/exercise1.11/exercise1.11.cpp
+++ ch1/exercise1.11/exercise1.11.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <utility>
 
-int main()
-{
-    std::cout <<  "Enter two numbers: " << std::endl;
+int main() {
+    std::cout << "Enter two integers: " << std::endl;
 
-    int left = 0, right = 0;
+    int left, right;
     std::cin >> left >> right;
-
     if (left > right) {
-        int tmp = left;
-	left = right;
-	right = tmp;
+	    std::swap(left, right);
     }
 
     while (left <= right) {
-        std::cout << left++ << " ";
+        std::cout << left;
+	if (left != right) {
+            std::cout << ", ";
+	}
+        ++left;
     }
     std::cout << std::endl;
 
