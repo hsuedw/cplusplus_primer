1616e787c5a99dc9eb6a29d1a1ce6e1bf6d85dc5
edward.hc.hsu <hsuedw@gmail.com>
2023-12-26 13:34:41 +0800

    Update the solution for Exercise 2.40

--- ch2/exercise2.40/exercise2.40.cpp
+++ ch2/exercise2.40/exercise2.40.cpp
@@ -2,9 +2,8 @@
 
 struct Sales_data {
     std::string bookNo;
-    unsigned units_sold;
-    double price;
-    double discount;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
 };
 
 int main()
